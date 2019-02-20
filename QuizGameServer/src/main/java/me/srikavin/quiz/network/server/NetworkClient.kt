ebd388db45c7f50cc3ad75fea0ab94b9c9a37635
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2019-02-20 18:36:56 -0500

    [WIP] Debug issue with receiving answers

--- QuizGameServer/src/main/java/me/srikavin/quiz/network/server/NetworkClient.kt
+++ QuizGameServer/src/main/java/me/srikavin/quiz/network/server/NetworkClient.kt
@@ -11,13 +11,13 @@ import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicBoolean
 
 data class NetworkClient(
-        override val id: UUID,
-        val socket: Socket,
-        val reader: BufferedInputStream,
-        val writer: BufferedOutputStream,
-        val buffer: ByteArrayOutputStream = ByteArrayOutputStream(),
-        var inProgress: Int = -1,
-        var total: Int = -1
+    override val id: UUID,
+    val socket: Socket,
+    val reader: BufferedInputStream,
+    val writer: BufferedOutputStream,
+    val buffer: ByteArrayOutputStream = ByteArrayOutputStream(),
+    var inProgress: Int = -1,
+    var total: Int = -1
 ) : BackingClient {
     internal val messageQueue: Queue<MessageBase> = ConcurrentLinkedQueue()
     internal val shouldKick = AtomicBoolean(false)
@@ -34,5 +34,20 @@ data class NetworkClient(
 
     override fun isConnected(): Boolean {
         return isConnected.get()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NetworkClient
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
     }
 }
